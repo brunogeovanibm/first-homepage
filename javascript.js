0b966498ef269a02032668406110cd750abf0a3e
bruno <bgeovani724@gmail.com>
2024-03-27 13:29:37 -0300

    css and html

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let firstName = "king"
+let lastName = "burger"
+let thisYear = 2024;
+let birthYear = 1969;
+let fullName =""+ lastName +" " + firstName + " "
+let age = thisYear - birthYear
+let greeting = "Hello, my name is " + fullName + " i am " + age + " years old!"
+console.log(greeting)
