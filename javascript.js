dcaa0ffab2f2c884df5279e100528b3de5d031d0
bruno <bgeovani724@gmail.com>
2024-03-27 18:50:04 -0300

    syling tweaks

--- javascript.js
+++ javascript.js
@@ -5,5 +5,5 @@ let birthYear = 1969;
 let fullName =""+ lastName +" " + firstName + " "
 let age = thisYear - birthYear
 let greeting = "Hello, my name is " + fullName + " i am " + age + " years old!"
-let hello= "ok you the king, burger king"
-alert(hello)
+let hello= "ok you the king, burger king" 
+alert(fullName)
